28ea8ffa9f65a27fb638447b5577ae0fba7d61a2
Alicia Washington <aliciawashington874@gmail.com>
2017-11-10 12:57:01 -0500

    added .gitignore .gitattributes

--- tippie/ViewController.swift
+++ tippie/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +26,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages=[0.1,0.15,0.2]
+        
+        let bill=Double(billField.text!) ?? 0
+        let tip=bill*tipPercentages[tipControl.selectedSegmentIndex]
+        let total=bill+tip
+        
+        tipLabel.text=String(format: "$%.2f",tip)
+        totalLabel.text=String(format: "$%.2f",total)
+        
+    }
+    
 }
-
